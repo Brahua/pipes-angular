d5fd3231a7f3bfd871c3b862d8e627dcbe7c1aca
Brahua <josuebh62@gmail.com>
2019-03-18 19:39:10 -0500

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Josue';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+  salario = 1234.5;
+
+  juegos = {
+    nombre: 'Resident Evil 1',
+    clave: 123,
+    edad: 30,
+    lugar: {
+      calle: 'Primera',
+      casa: '19'
+    }
+  };
+
+  fecha = new Date();
 }
